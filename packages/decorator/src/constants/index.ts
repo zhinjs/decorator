cf52e53eab6b6ea68f8aad7edb94e093bd09edad
Zero <1203970284@qq.com>
2023-08-27 14:02:29 +0800

    feat: update dependencies

---
+++ packages/decorator/src/constants/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @Date 2023-08-27 13:59:30
+ * @Author Zero 1203970284@qq.com
+ * @LastEditTime 2023-08-27 13:59:33
+ * @FilePath /Decorator/packages/decorator/src/constants/index.ts
+ * @Copyright (c) 2023 by Zero, All Rights Reserved.
+ */
---
+++ packages/decorator/src/decorators/command/command.decorator.ts
@@ -0,0 +1,7 @@
+/**
+ * @Date 2023-08-27 13:59:43
+ * @Author Zero 1203970284@qq.com
+ * @LastEditTime 2023-08-27 14:00:57
+ * @FilePath /Decorator/packages/decorator/src/decorators/command/command.decorator.ts
+ * @Copyright (c) 2023 by Zero, All Rights Reserved.
+ */
